a138c8f5d9c2fac57e961a15fd2975cffa2c7516
Boya-Bala-Lakshmi-Narasimha <boya96309@gmail.com>
2025-07-01 00:37:03 +0530

    Updated files and fixed dataset issues

--- img_preprocessing.py
+++ img_preprocessing.py
@@ -1,19 +1,15 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+datagen = ImageDataGenerator(rescale=1./255)
 
 train_data = datagen.flow_from_directory(
-    'dataset',
-    target_size=(150, 150),
-    batch_size=32,
+    'dataset',                # make sure it's 'dataset', not 'dataset/train'
+    target_size=(224, 224),
+    batch_size=16,
     class_mode='categorical',
     subset='training'
 )
 
-val_data = datagen.flow_from_directory(
-    'dataset',
-    target_size=(150, 150),
-    batch_size=32,
-    class_mode='categorical',
-    subset='validation'
-)
+val_data=train_data
+
+print("✅ Classes detected:", train_data.class_indices)
